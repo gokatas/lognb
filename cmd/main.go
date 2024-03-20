e692c5034fb5d922f9cf4355b3ca36a4dc0b6103
jozef <jozef.reisinger@gmail.com>
2024-03-20 15:13:26 +0100

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,55 @@
+// Log handles logging gracefully; the goroutines that do some important task
+// (like sleeping) will not block just because it's not possible to write logs.
+//
+// Start 10 goroutines each of which will be writing logs to a device. Simulate
+// a device problem by pressing Ctrl-C. Press Ctrl-C again to "fix" the problem.
+// Ctrl-\ will terminate the program (with a core dump).
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
+
+	"logger"
+)
+
+type device struct {
+	problem bool
+}
+
+func (d *device) Write(p []byte) (int, error) {
+	for d.problem {
+		time.Sleep(time.Second)
+	}
+	return fmt.Print(string(p))
+}
+
+func main() {
+	const grs = 10
+	var d device
+	// var l log.Logger // the standard logger is a blocking logger
+	// l.SetOutput(&d)
+	l := logger.New(&d, grs)
+
+	for i := 0; i < grs; i++ {
+		go func(id int) {
+			for {
+				l.Write(fmt.Sprintf("log from gr #%d", id))
+				doSomething()
+			}
+		}(i)
+	}
+
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, os.Interrupt)
+	for {
+		<-sigint
+		d.problem = !d.problem
+	}
+}
+
+func doSomething() {
+	time.Sleep(time.Second)
+}
---
+++ logger.go
@@ -0,0 +1,43 @@
+// Package logger uses channels to implement non-blocking
+// logging. Adapted from https://youtu.be/zDCKZn4-dck.
+package logger
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"sync"
+	"time"
+)
+
+type Logger struct {
+	logs chan string
+	wg   sync.WaitGroup
+}
+
+// New creates a logger that will write logs to w. Buf is the size of logs buffer.
+func New(w io.Writer, buf int) *Logger {
+	// New is sometimes called a factory function. It's useful
+	// when you need to initialize one or more fields of a type.
+	l := Logger{
+		logs: make(chan string, buf),
+	}
+
+	l.wg.Add(1)
+	go func() {
+		defer l.wg.Done()
+		for log := range l.logs {
+			fmt.Fprintf(w, "%s: %s\n", time.Now().Format(time.TimeOnly), log)
+		}
+	}()
+
+	return &l
+}
+
+func (l *Logger) Write(log string) {
+	select {
+	case l.logs <- log:
+	default:
+		fmt.Fprintf(os.Stderr, "%s: dropping logs\n", time.Now().Format(time.TimeOnly))
+	}
+}
